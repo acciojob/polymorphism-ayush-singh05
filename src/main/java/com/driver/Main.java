71746a6c75ef0b71e866b2a96fed0d285a825104
Ayush Singh <ayushsingh2442@gmail.com>
2023-06-09 10:26:15 +0530

    Polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -14,14 +14,10 @@ public class Main {
     }
     public static void main(String[] args) {
         Product p = new Product();
-        int pro = p.product(2,5);
-        System.out.println(pro);
 
-        int pro2 = p.product(5,6,7);
-        System.out.println(pro2);
-
-        double pro3 = p.product(2.5,4.4);
-        System.out.println(pro3);
+        System.out.println(p.product(2,5));
+        System.out.println(p.product(5,6,7));
+        System.out.println(p.product(2.5,4.4));
 
     }
 }
